a1ca029a6151f9d55ae8f29e6bda1192ece66252
Giovanny Carvalho <giovannyjcarvalho@gmail.com>
2021-09-24 21:49:59 -0300

    First Commit

---
+++ src/components/Display.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Display.css';
+
+ const display = props => {
+     return (
+        <div className="display">{props.value}</div>
+     )
+ }
+
+
+export default display
